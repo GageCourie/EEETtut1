a91d7dcf2fe2257b146ff896aa3ac7c047e76afb
Gage Courie <gagecourie@gmail.com>
2023-08-01 10:07:50 +0200

    Created and completed

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int a, b ,sum;
+
+	printf("Enter a value for a: ");
+	scanf("%d", &a);
+	
+	printf("Enter a value for b: ");
+	scanf("%d", &b);
+
+	sum = a+b;
+
+	printf("THe sum of and b is %d \n.", sum);
+
+}
